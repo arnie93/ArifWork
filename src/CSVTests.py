fed9ace3459e33ec3ccf04621aa30c1a65775e9b
arnie93 <khan.arif93@hotmail.com>
2020-03-01 13:40:44 -0500

    Task: Changes made to CSVTests file

--- src/CSVTests.py
+++ src/CSVTests.py
@@ -9,9 +9,9 @@ class MyTestCase(unittest.TestCase):
         self.csv_reader = CsvReader('/src/*.csv')
 
     def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_objects('person')
+        people = self.csv_reader.return_data_as_objects('Value 1')
         self.assertIsInstance(people, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
+        test_class = ClassFactory('Value 1', self.csv_reader.data[0])
         for person in people:
             self.assertEqual(person.__name__, test_class.__name__ )
 
