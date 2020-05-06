685d22928cbb90226694504ca53f484ffc2b48e1
Arif Khan <khan.arif93@hotmail.com>
2020-05-06 13:24:38 -0400

    Task: updated app.py

--- app/app.py
+++ app/app.py
@@ -44,7 +44,7 @@ def form_edit_get(car_id):
 @app.route('/edit/<int:car_id>', methods=['POST'])
 def form_update_post(car_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'), hw_id)
+    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'), car_id)
     sql_update_query = """UPDATE tblFordImport t SET t.Year= %s, t.Mileage = %s, t.Price = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
